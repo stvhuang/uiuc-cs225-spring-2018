e11ecb3f821605a81a3f84e3ac3d6e4884f25e9b
Yanjia Huang <yanjia.huang.tw@gmail.com>
2018-01-19 19:52:59 -0600

    Submission: mp1:Part 1 & Part 2

--- mp1/cs225/HSLAPixel.h
+++ mp1/cs225/HSLAPixel.h
@@ -2,12 +2,29 @@
  * @file HSLAPixel.h
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
-#ifndef CS225_HSLAPIXEL_H
-#define CS225_HSLAPIXEL_H
+#ifndef CS225_HSLAPIXEL_H_
+#define CS225_HSLAPIXEL_H_
+
+#include <iostream>
+#include <sstream>
 
 namespace cs225 {
+  class HSLAPixel {
+    public:
+    HSLAPixel ();
+    HSLAPixel (double hue, double saturation, double luminance);
+    HSLAPixel (double hue, double saturation, double luminance, double alpha);
+
+    double h;  // Hue
+    double s;  // Saturation
+    double l;  // Luminance
+    double a;  // Alpha
+
+    private:
+  };
 }
 
 #endif
--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -1,12 +1,44 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
 #include <string>
-
+#include <iostream>
 using namespace cs225;
 
 
 void rotate(std::string inputFile, std::string outputFile) {
   // Part 2
+  PNG inputImage;
+  inputImage.readFromFile(inputFile);
+
+  unsigned width = inputImage.width(), height = inputImage.height(),
+           output_x = 0, output_y = 0;
+
+  PNG outputImage(width, height);
+  std::cout << width << height;
+
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      //std::cout << x << ", " << y << std::endl << output_x << ", " << output_y << std::endl << std::endl;
+      output_x = width - x - 1;
+      output_y = height - y - 1;
+
+      HSLAPixel * input_pixel = inputImage.getPixel(x, y);
+      HSLAPixel * output_pixel = outputImage.getPixel(output_x, output_y);
+
+      output_pixel->h = input_pixel->h;
+      output_pixel->s = input_pixel->s;
+      output_pixel->l = input_pixel->l;
+      output_pixel->a = input_pixel->a;
+    }
+  }
+
+  int x =0, y =0;
+  HSLAPixel * input_pixel = inputImage.getPixel(x, y);
+  HSLAPixel * output_pixel = outputImage.getPixel(output_x, output_y);
+
+  //std::cout << "0, 0\n" << 
+
+  outputImage.writeToFile(outputFile);
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
