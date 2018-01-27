fd6dd99ccab19a379bdfc31bdf3eea7ecdd68e2e
Yan-Jia Jason Huang <yanjia.huang.tw@gmail.com>
2018-01-27 10:57:18 -0600

    Submission: mp1:part3

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -1,6 +1,7 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
 #include <string>
+#include <cstdlib>
 
 using namespace cs225;
 
@@ -36,6 +37,40 @@ void rotate(std::string inputFile, std::string outputFile) {
 PNG myArt(unsigned int width, unsigned int height) {
   PNG png(width, height);
   // Part 3
+  double myHue = 160.0;
+  double hue = 0.0;
+  unsigned y_end = 0;
+  int height_starts[9] = {   0,  200,  360,  460,  540,  590, 630, 662,  700};
+  int hue_mod[9] =       { 200,  160,  100,   80,   50,   40,  32,  16,    8};
+  double hue_times[9] =  {32.0, 24.0, 18.0, 16.0, 12.0, 10.0, 9.0, 5.0, 2.75};
+
+  for (int n = 0; n < 9; n++) {
+    for (unsigned x = 0; x < width; x++) {
+      if (n == 8) {
+        y_end = height;
+      } else {
+        y_end = height_starts[n+1];
+      }
+      for (unsigned y = height_starts[n]; y < y_end; y++) {
+        hue = myHue + (x / hue_mod[n]) * hue_times[n];
+        HSLAPixel &pixel = *png.getPixel(x, y);
+        pixel.h = hue;
+        pixel.s = 1.0;
+        pixel.l = 0.5;
+      }
+    }
+  }
+
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      HSLAPixel &pixel = *png.getPixel(x, y);
+      double r = (((double) rand() / (RAND_MAX)) + 1) * 20.0;
+      pixel.h += r;
+      if (pixel.h > 360.0) {
+        pixel.h = 360.0;
+      }
+    }
+  }
 
   return png;
 }
