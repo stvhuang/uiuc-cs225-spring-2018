1213f7a835aa707dc7101b1423e0283d47c40a94
Yan-Jia Huang <yanjia.huang.tw@gmail.com>
2018-01-31 17:40:41 -0600

    Submission: mp2 part1

--- mp2/Image.h
+++ mp2/Image.h
@@ -7,5 +7,27 @@
 #ifndef IMAGE_H_
 #define IMAGE_H_
 
+#include "cs225/PNG.h"
+
+namespace cs225 {
+class Image : public PNG {
+public:
+  Image();
+  Image(unsigned width, unsigned height);
+  void lighten();
+  void lighten(double amount);
+  void darken();
+  void darken(double amount);
+  void saturate();
+  void saturate(double amount);
+  void desaturate();
+  void desaturate(double amount);
+  void grayscale();
+  void rotateColor(double degrees);
+  void illinify();
+  void scale(double factor);
+  void scale(unsigned w, unsigned h);
+};
+} // namespace cs225
+
 #endif
- 
