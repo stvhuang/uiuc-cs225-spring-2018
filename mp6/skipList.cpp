304c61bd1e148aacb52c91f06dd6c1b0986935cb
Steven Huang <s@stvhuang.me>
2018-04-16 13:34:09 -0500

    Pass mp6: test_case 1 & 2

--- mp6/skipList.cpp
+++ mp6/skipList.cpp
@@ -11,8 +11,8 @@
  * Uses two sentinel nodes, each initially of height 1
  */
 SkipList::SkipList() {
-    SkipNode* head_ = new SkipNode();
-    SkipNode* tail_ = new SkipNode();
+    head_ = new SkipNode();
+    tail_ = new SkipNode();
 
     head_->nodePointers[0].next = tail_;
     tail_->nodePointers[0].prev = head_;
@@ -86,7 +86,7 @@ void SkipList::insert(int key, HSLAPixel value) {
         head_->nodePointers.push_back(SkipPointer());
         tail_->nodePointers.push_back(SkipPointer());
     }
-     
+
     this->listHeight_ = max(this->listHeight_, newNodeLevel);
 
     SkipNode * prev = traverse;
@@ -192,7 +192,7 @@ SkipNode * SkipList::findRHelper(int key, int level, SkipNode * curr) {
     if (nextKey > key) {
         ret = findRHelper(key, level, curr);
     } else {
-        ret = findRHelper(key, level, curr->nodePointers[level].next);    
+        ret = findRHelper(key, level, curr->nodePointers[level].next);
     }
 
     return NULL;
@@ -269,11 +269,10 @@ vector<int> SkipList::traverse() {
     vector<int> ret;
     SkipNode * listPrintingTraverser = head_;
 
-    while (listPrintingTraverser != tail_) {
+    while (listPrintingTraverser != NULL) {
         ret.push_back(listPrintingTraverser->key);
         listPrintingTraverser = listPrintingTraverser->nodePointers[0].next;
     }
 
     return ret;
 }
-
